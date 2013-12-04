54e80dcd0eec9d991f1d7bb5ef559f2397302cbc
YANG Fan <yangfan_fr@yahoo.fr>
2013-12-04 22:06:57 +0100

    correction bug compatibilite cocos2d et uikit

--- JudgeHelper/AppDelegate.m
+++ JudgeHelper/AppDelegate.m
@@ -72,64 +72,66 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    CGRect rect = [window_ bounds];
-    if(rect.size.width  < rect.size.height) {
-        rect.size = CGSizeMake(rect.size.height, rect.size.width);
-    }
-    CCGLView *glView = [CCGLView viewWithFrame:rect
-                                   pixelFormat:kEAGLColorFormatRGB565
-                                   depthFormat:0
-                            preserveBackbuffer:NO
-                                    sharegroup:nil
-                                 multiSampling:NO
-                               numberOfSamples:0];
-    
-    director_ = (CCDirectorIOS*) [CCDirector sharedDirector];
-	
-	director_.wantsFullScreenLayout = YES;
-    
-	// Display FSP and SPF
-	[director_ setDisplayStats:NO];
-	
-	// set FPS at 60
-	[director_ setAnimationInterval:1.0/60];
-    
-    // attach the openglView to the director
-    [director_ setView:glView];
-    
-	// 2D projection
-	[director_ setProjection:kCCDirectorProjection2D];
-	//	[director setProjection:kCCDirectorProjection3D];
-	
-	// Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
-	if( ! [director_ enableRetinaDisplay:YES] )
-		CCLOG(@"Retina Display Not supported");
-	
-	// Default texture format for PNG/BMP/TIFF/JPEG/GIF images
-	// It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
-	// You can change this setting at any time.
-	[CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
-	
-	// If the 1st suffix is not found and if fallback is enabled then fallback suffixes are going to searched. If none is found, it will try with the name without suffix.
-	// On iPad HD  : "-ipadhd", "-ipad",  "-hd"
-	// On iPad     : "-ipad", "-hd"
-	// On iPhone HD: "-hd"
-	CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
-	[sharedFileUtils setEnableFallbackSuffixes:NO];				// Default: NO. No fallback suffixes are going to be used
-	[sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
-	[sharedFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "ipad"
-	[sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
-	
-	// Assume that PVR images have premultiplied alpha
-	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
-    
-    [director_ setDelegate:self];
-    
-    //[self createIAd];
-    
     GlobalSettings *globalSettings = [GlobalSettings globalSettings];
     NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
     [globalSettings setDisplayMode: [((NSNumber *)[userDefaults objectForKey:@"displayMode"]) intValue]];
+    
+    if([globalSettings getDisplayMode] == COCOS2D) {
+        CGRect rect = [window_ bounds];
+        if(rect.size.width  < rect.size.height) {
+            rect.size = CGSizeMake(rect.size.height, rect.size.width);
+        }
+        CCGLView *glView = [CCGLView viewWithFrame:rect
+                                       pixelFormat:kEAGLColorFormatRGB565
+                                       depthFormat:0
+                                preserveBackbuffer:NO
+                                        sharegroup:nil
+                                     multiSampling:NO
+                                   numberOfSamples:0];
+        
+        director_ = (CCDirectorIOS*) [CCDirector sharedDirector];
+        
+        director_.wantsFullScreenLayout = YES;
+        
+        // Display FSP and SPF
+        [director_ setDisplayStats:NO];
+        
+        // set FPS at 60
+        [director_ setAnimationInterval:1.0/60];
+        
+        // attach the openglView to the director
+        [director_ setView:glView];
+        
+        // 2D projection
+        [director_ setProjection:kCCDirectorProjection2D];
+        //	[director setProjection:kCCDirectorProjection3D];
+        
+        // Enables High Res mode (Retina Display) on iPhone 4 and maintains low res on all other devices
+        if( ! [director_ enableRetinaDisplay:YES] )
+            CCLOG(@"Retina Display Not supported");
+        
+        // Default texture format for PNG/BMP/TIFF/JPEG/GIF images
+        // It can be RGBA8888, RGBA4444, RGB5_A1, RGB565
+        // You can change this setting at any time.
+        [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+        
+        // If the 1st suffix is not found and if fallback is enabled then fallback suffixes are going to searched. If none is found, it will try with the name without suffix.
+        // On iPad HD  : "-ipadhd", "-ipad",  "-hd"
+        // On iPad     : "-ipad", "-hd"
+        // On iPhone HD: "-hd"
+        CCFileUtils *sharedFileUtils = [CCFileUtils sharedFileUtils];
+        [sharedFileUtils setEnableFallbackSuffixes:NO];				// Default: NO. No fallback suffixes are going to be used
+        [sharedFileUtils setiPhoneRetinaDisplaySuffix:@"-hd"];		// Default on iPhone RetinaDisplay is "-hd"
+        [sharedFileUtils setiPadSuffix:@"-ipad"];					// Default on iPad is "ipad"
+        [sharedFileUtils setiPadRetinaDisplaySuffix:@"-ipadhd"];	// Default on iPad RetinaDisplay is "-ipadhd"
+        
+        // Assume that PVR images have premultiplied alpha
+        [CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
+        
+        [director_ setDelegate:self];
+        
+        //[self createIAd];
+    }
     
     self.rootViewController = [globalSettings getDisplayMode] == COCOS2D ? director_ : [MainViewController new];
     [self.navigationController setViewControllers:@[self.rootViewController] animated:NO];
